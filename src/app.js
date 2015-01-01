0d56c2f35b07d12031f3895ce8747e43a2d98863
Jordan <nospam@chaosserver.net>
2014-12-31 23:30:21 -0800

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,108 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var ajax = require('ajax');
+
+var splashCard = new UI.Card({
+  title: 'TCP Lighting',
+  icon: 'images/menu_icon.png',
+  body: 'Looking for hub...'
+});
+
+splashCard.show();
+tcpSceneMenu();
+
+function tcpSceneMenu() {
+  ajax(
+    {
+      url: 'http://lighting.local/gwr/gop.php',
+      method: 'post',
+      data: {
+        cmd: 'GWRBatch',
+        fmt: 'xml',
+        data: '<gwrcmds><gwrcmd><gcmd>SceneGetList</gcmd><gdata><gip><version>1</version><token>1234567890</token><islocal>1</islocal></gip></gdata></gwrcmd></gwrcmds>'
+      }
+    },
+    function(data) {
+      var sceneXmlArray = data.match(/<scene>.*?<\/scene>/g),
+          sceneItemsArray =[];
+    
+      console.log("Response is: " + data);
+      if(sceneXmlArray) {
+        sceneXmlArray.forEach(function(sceneXml) {
+          var sceneId = sceneXml.replace(/.*<sid>([0-9]*)<\/sid>.*/,"$1"),
+              sceneName = sceneXml.replace(/.*<name>(.*)<\/name>.*/,"$1"),
+              sceneItem = {
+                subtitle: sceneId,
+                title: sceneName
+              };
+        
+          sceneItemsArray.push(sceneItem);
+          console.log("1:" + sceneXml);
+          console.log(sceneId + ":" + sceneName);
+        });
+      
+        var sceneMenu = new UI.Menu({
+          sections: [{
+            title: 'Scenes',
+            items: sceneItemsArray
+          }]
+        });
+      
+        sceneMenu.on('select', function(e) {
+          console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+          console.log('The item is titled "' + e.item.title + '"');
+          console.log('The item is subtitled "' + e.item.subtitle + '"');
+          tcpChangeScene(e.item.subtitle);
+        });
+      
+        sceneMenu.show();
+        splashCard.hide();
+      } else {
+        displayError(1, "No available scenes.");
+      }
+    },
+    function(error) {
+      displayError(2, "Failed to get scene list: " + error);
+    }
+  );  
+}
+
+function tcpChangeScene(sceneId) {
+  var sceneCommand =
+      "<gip><version>1</version><token>1234567890</token><sid>" + sceneId + "</sid></gip>";
+  
+  ajax(
+    {
+      url: 'http://lighting.local/gwr/gop.php',
+      method: 'post',
+      data: {
+        cmd: 'SceneRun',
+        fmt: 'xml',
+        data: sceneCommand
+      }
+    },
+    function(data) {
+      
+    },
+    function(error) {
+      displayError(3, "Failed to set scene: " + error);
+    }
+  );
+}
+
+function displayError(errorCode, error) {
+  console.log("[" + errorCode + "] " + error);
+  var errorCard = new UI.Card({
+    title: 'Error',
+    subtitle: errorCode,
+    body: error,
+    scrollable: true
+  });  
+  errorCard.show();
+}
+
